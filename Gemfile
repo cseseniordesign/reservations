5c14b390767fed33fadaa0278734a2a98a362367
adobrusky <austindobrusky@gmail.com>
2022-11-08 21:17:59 -0600

    Added some basic unit tests
    Unit tests include verifying that valid/invalid logins work as expected and that the new members page loads.

--- Gemfile
+++ Gemfile
@@ -15,6 +15,9 @@ gem 'json'
 gem 'bigdecimal'
 gem 'rufus-scheduler'
 gem 'highlander'
+gem 'rack-test'
+gem 'rspec-core'
+gem "rspec-expectations"
 
 group :development do
   gem 'shotgun'
---
+++ config_setup.sh
@@ -0,0 +1,8 @@
+while getopts u:p: flag
+do 
+    case "${flag}" in
+        u) username=${OPTARG};;
+        p) password=${OPTARG};;
+    esac
+done
+echo "{\"database\":{\"adapter\":\"mysql2\",\"host\":\"cse.unl.edu\",\"username\":\"$username\",\"password\":\"$password\",\"database\":\"reservation\"}}" > config/config.json
