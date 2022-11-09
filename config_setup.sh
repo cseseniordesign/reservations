4e3e1b1e35893ab76b1d30b636927803009ec2b9
adobrusky <austindobrusky@gmail.com>
2022-11-09 14:28:03 -0600

    Updated setup script

--- config_setup.sh
+++ config_setup.sh
@@ -5,4 +5,5 @@ do
         p) password=${OPTARG};;
     esac
 done
+echo "user: $username, pass: $password"
 echo "{\"database\":{\"adapter\":\"mysql2\",\"host\":\"cse.unl.edu\",\"username\":\"$username\",\"password\":\"$password\",\"database\":\"reservation\"}}" > config/config.json
