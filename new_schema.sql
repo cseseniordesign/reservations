97e341e1de4151c02d1b9336bf70501577c47b08
Evan Mielak <evanmielak@gmail.com>
2022-11-14 18:17:25 -0600

    Added new database commands to new_schema.sql

--- new_schema.sql
+++ new_schema.sql
@@ -23,3 +23,17 @@ INSERT INTO `permissions` VALUES (9, 'Events Admin Read-only');
 
 -- Add category id to resources
 ALTER TABLE `resources` ADD COLUMN `category_id` int(11) NULL AFTER `name`;
+
+--Update-user-table-default-password
+ALTER TABLE `reservation`.`users` 
+CHANGE COLUMN `password_hash` `password_hash` VARCHAR(255) NULL DEFAULT '$2a$12$RGRjbe3xauqpFLf3eMskVOITvZgrHsDdZ/0zr03zxFNQM3k3HFOHS';
+
+--Add expiration_reminders-table
+CREATE TABLE `expiration_reminders` (
+  `ID` INT(11) NOT NULL AUTO_INCREMENT,
+  `first_reminder` INT(11) NOT NULL,
+  `second_reminder` INT(11) NOT NULL,
+  PRIMARY KEY (`ID`)
+) ENGINE=InnoDB AUTO_INCREMENT=1 DEFAULT CHARSET=latin1;
+
+INSERT INTO `reservation`.`expiration_reminders` (`ID`, `first_reminder`, `second_reminder`) VALUES ('1', '3', '0');
