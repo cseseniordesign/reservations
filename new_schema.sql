41f1cb7dbdcd5da7081b1643ab3a7bee3a7870e4
Josh Jacobs <54561791+jjacobs10@users.noreply.github.com>
2022-12-06 14:04:33 -0600

    Add script as per Austin's direction

--- new_schema.sql
+++ new_schema.sql
@@ -357,3 +357,6 @@ INSERT INTO `reservation`.`preset_events_has_resources` (`preset_events_id`, `re
 INSERT INTO `reservation`.`preset_events_has_resources` (`preset_events_id`, `resources_id`) VALUES ('27', '815');
 INSERT INTO `reservation`.`preset_events_has_resources` (`preset_events_id`, `resources_id`) VALUES ('28', '770');
 INSERT INTO `reservation`.`preset_events_has_resources` (`preset_events_id`, `resources_id`) VALUES ('28', '771');
+
+ALTER TABLE `reservation`.`events` 
+ADD COLUMN `trainer_confirmed` TINYINT NULL DEFAULT 0 AFTER `trainer_id`;
